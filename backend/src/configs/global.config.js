8c5e01d256593f5465cdc0db568d65279b28e517
ristovkiril <43649881+ristovkiril@users.noreply.github.com>
2024-01-15 22:26:28 +0100

    env file change

--- backend/src/configs/global.config.js
+++ backend/src/configs/global.config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export const PORT = 8080;
-export const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@coffee.vhcyiht.mongodb.net/?retryWrites=true&w=majority`;
+export const DB_URI = process.env.DB_URI;
 export const ROLE = {
   USER: "user",
   ADMIN: "admin"
